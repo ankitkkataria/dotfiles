b50ee4c40d6c8473283e4744504e09a7d27433e3
Ankit <96373845+ankitkkataria@users.noreply.github.com>
2024-08-14 02:07:27 +0530

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -14,16 +14,15 @@ vim.keymap.set("n", "J", "5j")
 vim.keymap.set("v", "J", "5j")
 vim.keymap.set("v", "K", "5k")
 vim.keymap.set("x", "p", [["_dP]])
-
+vim.keymap.set("n", "<leader>/",":noh<cr>")
+-- vim.keymap.set("n", "<C-b>","<leader>e")
+-- Copy/cut and paste to system clipboard
 vim.keymap.set({"n", "v"}, "y", [["+y]])
-vim.keymap.set("n", "Y", [["+Y]])
-
+vim.keymap.set({"n", "v"}, "Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "p", [["+p]])
 vim.keymap.set("n", "P", [["+P]])
-
 vim.keymap.set({"n", "v"}, "d", [["+d]])
-
-
+vim.api.nvim_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 -- EasyMotion Settings
 local hop = require('hop')
 vim.keymap.set('', 'f', function()
