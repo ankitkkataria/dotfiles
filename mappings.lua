62a90b5e7c8973b2973ec878c928d4f5c030a4ce
Ankit <96373845+ankitkkataria@users.noreply.github.com>
2024-08-18 15:18:15 +0530

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -15,9 +15,11 @@ vim.keymap.set("v", "J", "5j")
 vim.keymap.set("v", "K", "5k")
 vim.keymap.set("x", "p", [["_dP]])
 vim.keymap.set("n", "<leader>/",":noh<cr>")
+
 -- vim.keymap.set("n", "<C-b>","<leader>e")
 -- Copy/cut and paste to system clipboard
 vim.keymap.set({"n", "v"}, "y", [["+y]])
+vim.keymap.set({"n", "v"}, "mb", "%")
 vim.keymap.set({"n", "v"}, "Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "p", [["+p]])
 vim.keymap.set("n", "P", [["+P]])
@@ -31,3 +33,22 @@ end, {remap=true})
 vim.keymap.set('', '<leader>l', function()
   hop.hint_lines_skip_whitespace()
 end, {remap=true})
+
+-- Visual mode key mapping
+vim.keymap.set('v', '<C-d>', '<Plug>(VM-Find-Under)', { noremap = true, silent = true })
+
+-- Normal mode key mapping
+vim.keymap.set('n', '<C-d>', '<Plug>(VM-Find-Under)', { noremap = true, silent = true })
+
+-- Select text with Shift + Arrow keys in Insert mode
+vim.api.nvim_set_keymap('i', '<S-Left>', '<C-O>v<Left>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-Right>', '<C-O>v<Right>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-Up>', '<C-O>v<Up>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-Down>', '<C-O>v<Down>', { noremap = true, silent = true })
+
+-- Select text to the beginning and end of the line with Shift + Home/End in Insert mode
+vim.api.nvim_set_keymap('i', '<S-Home>', '<C-O>v<Home>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-End>', '<C-O>v<End>', { noremap = true, silent = true })
+
+-- Delete selected text with Backspace in Visual mode and go to Insert mode
+vim.api.nvim_set_keymap('v', '<BS>', 'dgi', { noremap = true, silent = true })
